0b20bdb23d37bf1ce4c43f52a013f9f3ef143a4e
duarte <duarte@DESKTOP-RAAQIQ9>
2024-11-13 18:39:05 +0000

    feat: adicionar classes Cliente e Pessoa com funcionalidades de exibição de informações
    - Adicionada a classe `Pessoa` com atributos como nome, data de nascimento, número de identificação, telefone e email.
    - Implementada a classe `Cliente` que herda de `Pessoa`, com um contador de clientes e número único para cada cliente.
    - Criada a exibição das informações do cliente com a classe `Cliente`, incluindo a informação herdada da classe `Pessoa`.
    - Atualização no `Program.cs` para criação de um objeto `Cliente` e exibição das suas informações.

--- GestaoAlojamentosTuristicos/Cliente.cs
+++ GestaoAlojamentosTuristicos/Cliente.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace GestaoAlojamentosTuristicos
 {
-    internal class Cliente
+    public class Cliente : Pessoa
     {
+        #region Attributes
+        static private int contadorClientes = 0;
+
+        private int numeroCliente;
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Constructors
+        public Cliente(
+            string nome,
+            DateTime dataNascimento,
+            string numeroIdentificacao,
+            string email,
+            string telefone)
+            : base(nome, dataNascimento, numeroIdentificacao, email, telefone)
+        {
+            numeroCliente = ++contadorClientes;
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Overrides
+        public override void ExibirInformacoes()
+        {
+            Console.WriteLine($"Numero do cliente: {numeroCliente}");
+
+            base.ExibirInformacoes();
+        }
+        #endregion
+
+        #region OtherMethods
+        #endregion
+
+        #region Deconstrutor
+        ~Cliente()
+        {
+        }
+        #endregion
     }
 }
--- GestaoAlojamentosTuristicos/Program.cs
+++ GestaoAlojamentosTuristicos/Program.cs
@@ -10,6 +10,20 @@ namespace GestaoAlojamentosTuristicos
     {
         static void Main(string[] args)
         {
+            // Criação de um Cliente
+            Cliente cliente = new Cliente(
+                "Duarte Pereira",
+                new DateTime(2003, 11, 12), // Data de nascimento
+                "271234567",
+                "+351 938123456",
+                "duarte.pereira@email.com"
+            );
+
+            // Mostrar as informações do Cliente
+            cliente.ExibirInformacoes();
+
+            // Pausa o programa para ver o resultado na consola
+            Console.ReadLine();
         }
     }
 }
